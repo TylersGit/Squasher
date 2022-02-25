c0dc8231f83e8c4b86cecee4f047a075f8ab0792
Tyler Jordan <1997.tylerjordan@gmail.com>
2022-02-26 00:23:08 +0200

    Started adding styles to the bug component

--- frontend/src/components/Bug.tsx
+++ frontend/src/components/Bug.tsx
@@ -1,30 +1,44 @@
 import React, { FC, PropsWithChildren } from "react";
+
 import BugProps from "../interfaces/BugInterfaces";
+import "../styles/Bug.css";
 
 const Bug: FC<BugProps> = (props: PropsWithChildren<BugProps>) => {
+  const isClosedStyle = props.closed
+    ? { display: "visible" }
+    : { display: "none" };
+
   return (
     <div>
       <div className="header">
-        <h1>{props.id}</h1>
+        <h2>{props.id}</h2>
         <h1>{props.title}</h1>
       </div>
-      <div>
-        <div className="people">
-          <p>{props.reporter}</p>
-          <p>{props.owner}</p>
+      <div className="body">
+        <div className="leftSide">
+          <div className="details">
+            <p>Status: {props.status}</p>
+            <p>Priority: {props.priority}</p>
+          </div>
+          <div className="description">
+            <p>Steps to reproduce: {props.stepsToReproduce}</p>
+            <p>Description: {props.description}</p>
+            <p>Expected result: {props.expectedResult}</p>
+            <p>Actual result: {props.actualResult}</p>
+          </div>
         </div>
-        <div className="details">
-          <p>{props.status}</p>
-          <p>{props.stepsToReproduce}</p>
-          <p>{props.priority}</p>
-          <p>{props.created}</p>
-          <p>{props.updated}</p>
-          <p>{props.closed}</p>
-        </div>
-        <div className="dscription">
-          <p>{props.expectedResult}</p>
-          <p>{props.actualResult}</p>
-          <p>{props.description}</p>
+        <div className="rightSide">
+          <div className="people">
+            <p className="header">People:</p>
+            <p>Reported by: {props.reporter}</p>
+            <p>Handled by: {props.owner}</p>
+          </div>
+          <div className="dates">
+            <p className="header">Dates:</p>
+            <p>Created: {props.created}</p>
+            <p>Updated: {props.updated}</p>
+            <p style={isClosedStyle}>Closed: {props.closed}</p>
+          </div>
         </div>
       </div>
       <div>
